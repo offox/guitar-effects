56fcf301e156a20124989ff130da9ce3fe81bd0b
Oswaldo Fratini <oswaldo.fratini@xpi.com.br>
2020-05-31 09:04:33 -0300

    Added copy-files-to-simul-and-soc.sh script

---
+++ copy-files-to-simul-and-soc.sh
@@ -0,0 +1,26 @@
+cp altfp_exp_ge.v ../SoC/DE0_NANO_SOC_GHRD/soc_system/synthesis/submodules 
+cp altfp_mult_ge.v ../SoC/DE0_NANO_SOC_GHRD/soc_system/synthesis/submodules 
+cp altp_sub_ge.v ../SoC/DE0_NANO_SOC_GHRD/soc_system/synthesis/submodules 
+cp avl_dram.v ../SoC/DE0_NANO_SOC_GHRD/soc_system/synthesis/submodules 
+cp distortion.v ../SoC/DE0_NANO_SOC_GHRD/soc_system/synthesis/submodules 
+cp guitar_effects.v ../SoC/DE0_NANO_SOC_GHRD/soc_system/synthesis/submodules 
+cp ram_port_2.v ../SoC/DE0_NANO_SOC_GHRD/soc_system/synthesis/submodules 
+cp fifo_ge.v ../SoC/DE0_NANO_SOC_GHRD/soc_system/synthesis/submodules 
+
+cp altfp_exp_ge.v ../SoC/DE0_NANO_SOC_GHRD 
+cp altfp_mult_ge.v ../SoC/DE0_NANO_SOC_GHRD 
+cp altp_sub_ge.v ../SoC/DE0_NANO_SOC_GHRD 
+cp avl_dram.v ../SoC/DE0_NANO_SOC_GHRD 
+cp distortion.v ../SoC/DE0_NANO_SOC_GHRD 
+cp guitar_effects.v ../SoC/DE0_NANO_SOC_GHRD 
+cp ram_port_2.v ../SoC/DE0_NANO_SOC_GHRD
+cp fifo_ge.v ../SoC/DE0_NANO_SOC_GHRD
+
+cp altfp_exp_ge.v simulation/modelsim
+cp altfp_mult_ge.v simulation/modelsim
+cp altp_sub_ge.v simulation/modelsim
+cp avl_dram.v simulation/modelsim
+cp distortion.v simulation/modelsim
+cp guitar_effects.v simulation/modelsim
+cp ram_port_2.v simulation/modelsim
+cp fifo_ge.v simulation/modelsim
